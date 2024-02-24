b635202b5228dc3e3a4fc7c5b8a89473756027ca
Safari Germain <99614251+germainsafari@users.noreply.github.com>
2024-02-24 22:56:06 +0100

    Update Learnkeras.py

--- Learnkeras.py
+++ Learnkeras.py
@@ -1,1 +1,13 @@
 
+from keras.models import Sequential
+from keras.layers import Dense
+#create model
+model = Sequential()
+
+#get number of columns in training data
+n_cols = train_X.shape[1]
+ 
+#add model layers
+model.add(Dense(10, activation='relu', input_shape=(n_cols,)))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1))
